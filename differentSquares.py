fca41d3807352ec25afee0b331ce9c928f29c219
Ryan Smith <rksmith369@gmail.com>
2018-12-23 14:41:22 -0800

    first commit

---
+++ differentSquares.py
@@ -0,0 +1,50 @@
+"""Given a rectangular matrix containing only digits, calculate the number of different 2 × 2 squares in it.
+
+Example
+
+For
+
+matrix = [[1, 2, 1],
+          [2, 2, 2],
+          [2, 2, 2],
+          [1, 2, 3],
+          [2, 2, 1]]
+the output should be
+differentSquares(matrix) = 6.
+
+Here are all 6 different 2 × 2 squares:
+
+1 2
+2 2
+2 1
+2 2
+2 2
+2 2
+2 2
+1 2
+2 2
+2 3
+2 3
+2 1
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.array.integer matrix
+
+Guaranteed constraints:
+1 ≤ matrix.length ≤ 100,
+1 ≤ matrix[i].length ≤ 100,
+0 ≤ matrix[i][j] ≤ 9.
+
+[output] integer
+
+The number of different 2 × 2 squares in matrix.
+"""
+
+def differentSquares(matrix):
+    s = set()
+    for i in range(len(matrix)-1):
+        for j in range(len(matrix[0]) - 1):
+            s.add(str(matrix[i][j]) + ' ' + str(matrix[i][j+1]) + ' ' +str(matrix[i+1][j]) + ' ' + str(matrix[i+1][j+1]))
+    return len(s)
